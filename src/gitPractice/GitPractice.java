dee00cc8177b6a70598ebf7fa8f0f6f1d6cd7a58
zahid khan <zahid00nyc@gmail.com>
2022-02-13 13:17:24 -0500

    zahid updated

--- src/gitPractice/GitPractice.java
+++ src/gitPractice/GitPractice.java
@@ -5,6 +5,7 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("My Name is TalenTech");
 		System.out.println("What's your name?");
+		System.out.println("My Name is Zahid");
 
 	}
 
